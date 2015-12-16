d5b5abaaa34ac31a5fdd4502a689f236b2069499
Fynn Schmidt <182365@helenpark.nl>
2015-12-16 13:50:15 +0100

    initiate

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
-<?php
- echo 'hello';
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<?php 
+$x = file("random");
+print_r($x);
